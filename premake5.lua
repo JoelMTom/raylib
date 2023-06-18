ad68be364c3a6359e7ae94b4e7e2b6389c58647b
JoelMTom <joelqwerty007@gmail.com>
2023-06-18 23:23:23 +0530

    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+baseName = path.getbasename(os.getcwd())
+
+project (baseName)
+kind "StaticLib"
+language "C"
+staticruntime "off"
+
+	targetdir "%{wks.location}/build/bin/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/dependencies/%{prj.name}" 
+	objdir "%{wks.location}/build/bin-int/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/dependencies/%{prj.name}" 
+
+files {
+	"src/*.h",
+	"src/*.c",
+}
+
+
+includedirs {
+	"src/external/glfw/include",
+}
+
+defines 
+{
+	"NDEBUG",
+	"PLATFORM_DESKTOP",
+	"GRAPHICS_API_OPENGL_33",
+	"_CRT_SECURE_NO_WARNINGS",
+}
+
+filter "system:windows"
+
